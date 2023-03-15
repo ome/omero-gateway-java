ecb8157ffdcb28d7358662be31a6f28ee6bb563d
Dominik Lindner <dominikl@users.noreply.github.com>
2023-03-15 10:11:50 +0000

    Update src/main/java/omero/gateway/facility/LoadFacility.java
    Co-authored-by: jean-marie burel <j.burel@dundee.ac.uk>

--- src/main/java/omero/gateway/facility/LoadFacility.java
+++ src/main/java/omero/gateway/facility/LoadFacility.java
@@ -1,6 +1,6 @@
 /*
  *------------------------------------------------------------------------------
- *  Copyright (C) 2022 University of Dundee. All rights reserved.
+ *  Copyright (C) 2023 University of Dundee. All rights reserved.
  *
  *
  *  This program is free software; you can redistribute it and/or modify
