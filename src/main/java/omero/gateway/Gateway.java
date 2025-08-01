50db38543bef5df2ac9a16caa1430a076af99348
Jean-Marie Burel <j.burel@dundee.ac.uk>
2025-08-01 08:05:37 +0100

    Fix remaining import

--- src/main/java/omero/gateway/Gateway.java
+++ src/main/java/omero/gateway/Gateway.java
@@ -41,7 +41,7 @@ import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import ome.formats.OMEROMetadataStoreClient;
 import omero.RType;
