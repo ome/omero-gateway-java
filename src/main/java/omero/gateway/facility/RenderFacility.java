b5d54fe59da39136dac978db9c1b8dbdebd99ab6
Dominik Lindner <dominikl@users.noreply.github.com>
2026-01-29 12:08:59 +0000

    Update src/main/java/omero/gateway/facility/RenderFacility.java
    Co-authored-by: jean-marie burel <j.burel@dundee.ac.uk>

--- src/main/java/omero/gateway/facility/RenderFacility.java
+++ src/main/java/omero/gateway/facility/RenderFacility.java
@@ -1,6 +1,6 @@
 /*
  *------------------------------------------------------------------------------
- *  Copyright (C) 2024 University of Dundee. All rights reserved.
+ *  Copyright (C) 2025 University of Dundee. All rights reserved.
  *
  *
  *  This program is free software; you can redistribute it and/or modify
